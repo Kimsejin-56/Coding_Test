0aefe4e5f77e1cb527e6ad78fbe9e97b2f51af11
Kimsejin-56 <saj5877@naver.com>
2026-01-04 18:18:54 +0900

    [level 0] Title: 코드 처리하기, Time: 340.76 ms, Memory: 381 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String solution(String code) {
+        int mode = 0;
+        int idx = 0;
+        String answer = "";
+        
+        for(int i =0; i< code.length(); i++) {
+            if(mode == 0){
+                if(code.charAt(i) != '1'){
+                    if(i % 2 == 0) answer += code.charAt(i);
+                } else {
+                    mode = 1; 
+                }
+            } else {
+                if(code.charAt(i) != '1'){
+                    if(i % 2 == 1) answer += code.charAt(i);
+                } else {
+                    mode = 0;
+                }
+            } 
+        }
+        
+        if(answer == "") return "EMPTY";
+        
+        return answer;
+    }
+}
