a9da635e460c60c622f8e5086e4cc953baebe6d8
Kimsejin-56 <saj5877@naver.com>
2026-01-04 20:53:12 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 234.37 ms, Memory: 377 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -4,13 +4,11 @@ class Solution {
     
         for(int i=0; i<queries.length; i++){
             int [] query = queries[i];
-            int n1 = query[0];
-            int n2 = query[1];
             int idx = 0;
             String temp = "";
             String answer = "";
             
-            for(int j=n1; j<=n2; j++){
+            for(int j=query[0]; j<=query[1]; j++){
                 temp += result.charAt(j); 
             }
  
