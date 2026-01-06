a2daf1b8dc65229ca48fddb21e58cc05eae7fd05
Kimsejin-56 <saj5877@naver.com>
2026-01-06 15:57:48 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 1.76 ms, Memory: 97.6 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
@@ -3,30 +3,32 @@ import java.util.*;
 class Solution {
     public int[][] solution(int[][] arr) {
         int [][] answer;
-
+        
         if(arr.length >= arr[0].length){
             answer = new int[arr.length][arr.length];
+            
         } else {
             answer = new int[arr[0].length][arr[0].length];
         }
-
-        for(int i = 0; i < arr.length; i++){
+        
+        
+        for(int i=0; i<arr.length; i++){
             int[] temp = arr[i];
-
-            if(temp.length != answer.length){
-                if(answer.length > temp.length){
-                    for(int j = 0; j < answer.length - temp.length; j++){
-                        temp = Arrays.copyOf(temp, temp.length + 1);
-                        temp[temp.length - 1] = 0;
+            
+            if(temp.length != arr.length){
+                if(arr.length > temp.length){
+                    for(int j=0; j<arr.length-temp.length; j++){
+                        temp = Arrays.copyOf(temp, temp.length+1);
+                        temp[temp.length-1] = 0;
                     }
                 }
             }
-
-            for(int k = 0; k < temp.length; k++){
-                answer[i][k] = temp[k];
+            
+            for(int j=0; j<temp.length; j++){
+                answer[i][j] = temp[j];
             }
         }
-
+        
         return answer;
     }
 }
