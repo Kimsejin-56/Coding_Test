ead90fbb7c23bcd65c3fcb03af48c2f9aecd595d
Kimsejin-56 <saj5877@naver.com>
2026-01-06 16:41:44 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 15.71 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = "";
+        
+        for(int i=0; i<s; i++){
+            answer += my_string.charAt(i);        
+        }
+        
+        for(int i=0; i<overwrite_string.length(); i++){
+            answer += overwrite_string.charAt(i);
+        }
+        
+        int idx = answer.length();
+        int diff = my_string.length()-answer.length();
+        
+        if(my_string.length() > answer.length()){
+            for(int i=0; i<diff; i++){
+                answer += my_string.charAt(idx++);
+            }
+        }
+        
+        return answer;
+    }
+}
