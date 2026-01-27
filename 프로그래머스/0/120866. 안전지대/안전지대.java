81d2c4e3d8fb7c566e73742d8898f0b169fcbc17
Kimsejin-56 <saj5877@naver.com>
2026-01-27 21:11:53 +0900

    [level 0] Title: 안전지대, Time: 0.04 ms, Memory: 83.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -0,0 +1,46 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] board) {
+        int answer = 0;
+        int len =board.length;
+        int[][] arr = new int[len+2][len+2];
+        
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr.length; j++){
+                if(i == 0) arr[i][j] = -1;
+                if(j == 0) arr[i][j] = -1;
+                if(i == len+1) arr[i][j] = -1;
+                if(j == len+1) arr[i][j] = -1;
+            }
+        }
+        
+        for(int i=1; i<=len; i++){
+            for(int j=1; j<=len; j++){
+                arr[i][j]=board[i-1][j-1];
+            }
+        }
+        
+        for(int i=1; i<=len; i++){
+            for(int j=1; j<=len; j++){
+                if(board[i-1][j-1] == 1){
+                    arr[i+1][j] = -1;
+                    arr[i-1][j] = -1;
+                    arr[i][j+1] = -1;
+                    arr[i][j-1] = -1;
+                    arr[i+1][j+1] = -1;
+                    arr[i+1][j-1] = -1;
+                    arr[i-1][j+1] = -1;
+                    arr[i-1][j-1] = -1;
+                }
+            }
+        }
+        
+        for(int i=1; i<=len; i++){
+            for(int j=1; j<=len; j++){
+                if(arr[i][j] == 0) answer++; 
+            }
+        }
+        
+        return answer;
+    }
+}
