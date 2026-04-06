bbcc0dc2eaf204e711330283e98ea16b216eb04a
Kimsejin-56 <saj5877@naver.com>
2026-04-06 15:23:44 +0900

    [Gold IV] Title: 알파벳, Time: 1180 ms, Memory: 297412 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,3 +1,5 @@
+
+
 import java.util.*;
 
 class Point{
@@ -13,26 +15,23 @@ class Point{
 public class Main {
     static int r, c, answer;
     static char[][] arr;
-    static List<Character> pm = new ArrayList<>();
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, -1, 0, 1};
     static boolean[] alpha = new boolean[26];
 
     static void dfs(int L, Point p) {
         alpha[arr[p.x][p.y] - 'A'] = true;
+        answer = Math.max(answer, L);
 
         for (int i = 0; i < 4; i++) {
             int nx = p.x + dx[i];
             int ny = p.y + dy[i];
 
             if (nx >= 0 && ny >= 0 && nx < r && ny < c && !alpha[arr[nx][ny]-'A']) {
-                pm.add(arr[nx][ny]);
                 dfs(L + 1, new Point(nx, ny));
                 alpha[arr[nx][ny] - 'A'] = false;
-                pm.remove(L+1);
             }
         }
-        answer = Math.max(answer, pm.size());
     }
 
     public static void main(String[] args) {
@@ -49,8 +48,7 @@ public class Main {
             }
         }
 
-        pm.add(arr[0][0]);
-        dfs(0, new Point(0, 0));
+        dfs(1, new Point(0, 0));
         System.out.println(answer);
     }
 }
