db3f50667bcc007c6c0c74c98118a0441cd41648
Kimsejin-56 <saj5877@naver.com>
2026-04-28 18:43:36 +0900

    [level 3] Title: 여행경로, Time: 0.44 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,18 +1,37 @@
 import java.util.*;
 
+class Point implements Comparable<Point> {
+    String s, e;
+    public Point(String s, String e){
+        this.s=s;
+        this.e=e;
+    }
+    
+    public int compareTo(Point p){
+        return this.e.compareTo(p.e);
+    }
+}
 class Solution {
+    static List<Point> list;
     static String[] answer;
+    static boolean flag;
     
     public String[] solution(String[][] tickets) {
         answer = new String[tickets.length+1];
         boolean[] visited = new boolean[tickets.length];
+        list=new ArrayList<>();
         
         for(int i=0; i<tickets.length; i++){
-            if(tickets[i][0].equals("ICN")){
+            list.add(new Point(tickets[i][0], tickets[i][1]));
+        }
+        Collections.sort(list);
+        
+        for(int i=0; i<list.size(); i++){
+            Point p=list.get(i);
+            if(p.s.equals("ICN")) {
                 visited[i]=true;
-                String[] arr = new String[tickets.length+1];
-                arr[0]=tickets[i][0];
-                dfs(visited, tickets, 1, tickets[i][1], arr);
+                answer[0]=p.s;
+                dfs(visited, p, 1);
                 visited[i]=false;
             }
         }
@@ -20,29 +39,19 @@ class Solution {
         return answer;
     }
     
-    public void dfs(boolean[] visited, String[][] tickets, int depth, String str, String[] arr){
-        if(depth==tickets.length){
-            arr[depth]=str;
-            
-            if(answer[0]==null) {
-                for(int i=0; i<answer.length; i++) answer[i]=arr[i];
-            } else {
-                System.out.println("check");
-                for(int i=0; i<answer.length; i++){
-                    if(!arr[i].equals(answer[i])){
-                        if(arr[i].compareTo(answer[i]) < 0){
-                            for(int j=0; j<answer.length; j++) answer[j]=arr[j];
-                        }
-                        return;
-                    }
-                }
-            }
+    public void dfs(boolean[] visited, Point p, int depth){
+        if(flag) return;
+        if(depth==list.size()){
+            answer[depth]=p.e;
+            flag=true;
+            return;
         }else {
-            for(int i=0; i<tickets.length; i++){
-                if(str.equals(tickets[i][0]) && !visited[i]){
+            for(int i=0; i<list.size(); i++){
+                Point np=list.get(i);
+                if(!visited[i] && p.e.equals(np.s)){
                     visited[i]=true;
-                    arr[depth]=tickets[i][0];
-                    dfs(visited, tickets, depth+1, tickets[i][1], arr);
+                    answer[depth]=np.s;
+                    dfs(visited, np, depth+1);
                     visited[i]=false;
                 }
             }
