e33e7a8eac530453047bef78af4fb3a5eb980717
Kimsejin-56 <saj5877@naver.com>
2026-04-28 15:19:57 +0900

    [level 3] Title: 네트워크, Time: 0.73 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,45 +1,33 @@
 import java.util.*;
-class Point{
-    int x,y;
-    public Point(int x, int y){
-        this.x=x;
-        this.y=y;
-    }
-}
 
 class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean[][] visited=new boolean[n][n];
+        boolean[] visited=new boolean[n];
         
         for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(computers[i][j]==1 && !visited[i][j]){
-                    bfs(visited, computers, new Point(i, j));
-                    answer++;
-                }
+            if(!visited[i]) {
+                bfs(visited, computers, i);
+                answer++;
             }
         }
-        
         
         return answer;
     }
     
-    public void bfs(boolean[][] visited, int[][] computers, Point cur){
-        Queue<Point> q = new ArrayDeque<>();
+    public void bfs(boolean[] visited, int[][] computers, int cur){
+        Queue<Integer> q = new ArrayDeque<>();
         q.offer(cur);
-        visited[cur.x][cur.y]=true;
+        visited[cur]=true;
         
         while(!q.isEmpty()){
-            Point p=q.poll();
-            // System.out.println(p.x+" "+p.y);
+            int x = q.poll();
             for(int i=0; i<computers.length; i++){
-                if(computers[p.x][i]==1 && !visited[p.x][i]){
-                    visited[p.x][i]=true;
-                    q.offer(new Point(i, p.x));
+                if(computers[x][i]==1 && !visited[i]){
+                    visited[i]=true;
+                    q.offer(i);
                 }
             }
         }
-         // System.out.println();
     }
 }
