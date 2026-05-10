ec959ab7770aa75be318bb5e47e2079232e5ff8f
Kimsejin-56 <saj5877@naver.com>
2026-05-11 01:52:55 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.12 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n) {
+        if(n==1) return 1;
+        long[] jump=new long[n+1];
+        jump[1]=1;
+        jump[2]=2;
+        
+        for(int i=3; i<=n; i++){
+            jump[i]=(jump[i-1]+jump[i-2])%1234567;
+        }
+        
+        return jump[n];
+    }
+}
