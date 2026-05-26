3aff5c66d02369f47128761c5dbac780cc60de86
Kimsejin-56 <saj5877@naver.com>
2026-05-26 14:25:35 +0900

    [level 1] Title: 카드 뭉치, Time: 0.04 ms, Memory: 60.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String answer = "Yes";
+        int c1=0, c2=0;
+        
+        
+        for(int i=0; i<goal.length; i++){
+            if(c1<cards1.length && goal[i].equals(cards1[c1])){
+                c1++;
+                continue;
+            }
+            
+            if(c2<cards2.length && goal[i].equals(cards2[c2])){
+                c2++;
+                continue;
+            }
+            
+            return "No";
+        }
+        
+        return answer;
+    }
+}
