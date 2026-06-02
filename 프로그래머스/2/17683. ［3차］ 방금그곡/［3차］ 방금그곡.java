5ab1a3099529359b46e93e6c693ee6ea9b357452
Kimsejin-56 <saj5877@naver.com>
2026-06-02 13:35:23 +0900

    [level 2] Title: [3차] 방금그곡, Time: 11.28 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String m, String[] musicinfos) {
+        String answer = "(None)";
+        int max=0;
+        
+        for(int i=0; i<musicinfos.length; i++){
+            String[] arr=musicinfos[i].split(",");
+            int time=getTime(arr[0], arr[1]);
+            String str=convert(arr[3]);
+            
+            System.out.println(str);
+            
+            String music="";
+            int idx=0;
+            for(int j=0; j<time; j++){
+                idx=j % str.length();
+                music+=String.valueOf(str.charAt(idx));
+            }
+            
+            m=convert(m);
+            if(music.contains(m)){
+                if(max < time){
+                    max=time;
+                    answer=arr[2];
+                }
+            }
+        }
+        
+        return answer;
+    }
+    
+    public int getTime(String s, String e){
+        String[] sarr=s.split(":");
+        String[] earr=e.split(":");
+        
+        int sh=Integer.parseInt(sarr[0]);
+        int sm=Integer.parseInt(sarr[1]);
+        int eh=Integer.parseInt(earr[0]);
+        int em=Integer.parseInt(earr[1]);
+        
+        return (eh-sh)*60 + (em-sm);
+    }
+    
+    
+    public String convert(String s){
+        s=s.replace("A#", "a");
+        s=s.replace("C#", "c");
+        s=s.replace("D#", "d");
+        s=s.replace("F#", "f");
+        s=s.replace("G#", "g");
+        return s;
+    }
+}
