83038d000083d5aa584ac74e6f6caa1d1e731ff0
Kimsejin-56 <saj5877@naver.com>
2026-06-20 01:00:25 +0900

    [level 1] Title: [1차] 다트 게임, Time: 4.27 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String dartResult) {
+        int answer = 0;
+        int[] arr=new int[3];
+        int idx=0;
+        int n=0;
+        String num="";
+        
+        for(int i=0; i<dartResult.length(); i++){
+            char pre=dartResult.charAt(i);
+            
+            if(Character.isDigit(pre)) num+=pre;
+            else if(pre=='S' || pre=='D' || pre=='T'){
+                n=Integer.parseInt(num);
+                num="";
+                if(pre=='D') n=(int)Math.pow(n,2);
+                else if(pre=='T') n=(int)Math.pow(n,3);
+                
+                arr[idx++]=n;
+            } else{
+                if(pre=='*'){
+                    if(idx==1) arr[idx-1]*=2;
+                    else {
+                        arr[idx-2]*=2;
+                        arr[idx-1]*=2;
+                        continue;
+                    }
+                }else if(pre=='#'){
+                    arr[idx-1]= -arr[idx-1];
+                    continue;
+                }
+            }
+        }
+        
+        for(int i=0; i<3; i++) answer+=arr[i];
+        
+        return answer;
+    }
+}
+
