c1960aee2ca18ba3d4080678d1cf806edabb46f4
Kimsejin-56 <saj5877@naver.com>
2026-08-03 19:15:35 +0900

    [level 1] Title: 음양 더하기, Time: 0.04 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        int len=absolutes.length;
+        for(int i=0; i<len; i++){
+            int num=absolutes[i];
+
+            if(signs[i]) answer+=num;
+            else answer-=num;
+        }
+            return answer;
+    }
+}
