0125e781e7d7ad4c1f81a51dea99da375ae8c0da
Kimsejin-56 <saj5877@naver.com>
2026-08-03 19:56:36 +0900

    [level 2] Title: 괄호 회전하기, Time: 147.81 ms, Memory: 342 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -26,20 +26,14 @@ class Solution {
         for(int i=0; i<s.length(); i++){
             char c=s.charAt(i);
             
-            if(c=='(' || c=='[' || c=='{'){
-                stack.push(c);
-            }else if(stack.isEmpty()) return false;
-            else if(c==')') {
-                if(stack.pop()!='(') return false;
-            }else if(c=='}') {
-                if(stack.pop()!='{') return false;
-            }else if(c==']') {
-                if(stack.pop()!='[') return false;
+            if(c=='(') stack.push(')');
+            else if(c=='{') stack.push('}');
+            else if(c=='[') stack.push(']');
+            else{
+                if(stack.isEmpty() || stack.pop()!=c) return false;
             }
         }
         
-        if(!stack.isEmpty()) return false;
-        
-        return true;
+        return stack.isEmpty();
     }
 }
