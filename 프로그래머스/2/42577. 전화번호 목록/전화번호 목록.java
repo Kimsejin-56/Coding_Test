d714b65b0426685878909d8787e60e683fe4d634
Kimsejin-56 <saj5877@naver.com>
2026-08-15 15:49:59 +0900

    [level 2] Title: 전화번호 목록, Time: 206.00 ms, Memory: 231 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -4,23 +4,15 @@ class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
         HashMap<String, String> map=new HashMap<>();
-        Arrays.sort(phone_book);
         
+        for(String s : phone_book) map.put(s, s);
         
-        for(int i=0; i<phone_book.length-1; i++){
+        for(int i=0; i<phone_book.length; i++){
             String s=phone_book[i];
-            String next=phone_book[i+1];
-            
-            int len=Math.min(s.length(), next.length());
-            
-            int cnt=len;
-            for(int j=0; j<len; j++){
-                if(s.charAt(j)==next.charAt(j)) cnt--;
-            }
-            
-            if(cnt==0){
-                answer=false;
-                break;
+            String str="";
+            for(int j=0; j<s.length()-1; j++){
+                str+=s.charAt(j);
+                if(map.get(str)!=null) return false;
             }
         }
         
