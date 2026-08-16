55cf0aa3abbcf9e6a141dbd26d0ae447cdca3d0f
Kimsejin-56 <saj5877@naver.com>
2026-08-16 14:58:13 +0900

    [level 2] Title: 모음 사전, Time: 4.21 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -3,27 +3,26 @@ import java.util.*;
 class Solution {
     static String[] arr={"A", "E", "I", "O", "U"};
     static int cnt=1;
-    static int answer;
     
     public int solution(String word) {
-        answer = 0;
-        
-        dfs(word, 0, "");
+        int answer = 0;
+        answer=dfs(word, 0, "");
         return answer;
     }
     
-    public void dfs(String s, int depth, String str){
+    public int dfs(String s, int depth, String str){
         if(depth==5){
-            return;
+            return -1;
         }else{
             for(int i=0; i<5; i++){
                 if(s.equals(str+arr[i])){
-                    answer=cnt;
-                    return;
+                    return cnt;
                 }
                 cnt++;
-                dfs(s, depth+1, str+arr[i]);
+               int result=dfs(s, depth+1, str+arr[i]);
+                if(result!=-1) return cnt;
             }
         }
+        return -1;
     }
 }
